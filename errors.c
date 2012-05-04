c13ceebf3bcadbb6bb3e6bd86c6b803f826ac80c
Andrei Jianu <andreisystem@gmail.com>
2012-05-04 08:42:52 +0200

    changed sprintf to snprintf

--- errors.c
+++ errors.c
@@ -3,38 +3,38 @@
 void badRequest(int client)
 {
 	int sz = 0;
-	char buf[1024];
-	memset(buf, 0, 1024);
+	char buf[MAX_BUFFER_ERROR];
+	memset(buf, 0, MAX_BUFFER_ERROR);
 
-	sz = sprintf(buf, "HTTP/1.0 400 Bad Request\r\n");
+	sz = snprintf(buf, MAX_BUFFER_ERROR, "HTTP/1.0 400 Bad Request\r\n");
 	send(client, buf, sz, 0);
 	
-	sz = sprintf(buf, "Content-type: text/html\r\n");
+	sz = snprintf(buf, MAX_BUFFER_ERROR, "Content-type: text/html\r\n");
 	send(client, buf, sz, 0);
 	
-	sz = sprintf(buf, "\r\n");
+	sz = snprintf(buf, MAX_BUFFER_ERROR, "\r\n");
 	send(client, buf, sz, 0);
 	
-	sz = sprintf(buf, "<html><title>Bad Request</title><body><p>Your browser sent a bad request</p></body></html>");
+	sz = snprintf(buf, MAX_BUFFER_ERROR, "<html><title>Bad Request</title><body><p>Your browser sent a bad request</p></body></html>");
 	send(client, buf, sz, 0);
 }
 
 void notFound(int client)
 {
 	int sz = 0;
-	char buf[1024];
-	memset(buf, 0, 1024);
+	char buf[MAX_BUFFER_ERROR];
+	memset(buf, 0, MAX_BUFFER_ERROR);
 
-	sz = sprintf(buf, "HTTP/1.0 404 File Not Found\r\n");
+	sz = snprintf(buf, MAX_BUFFER_ERROR, "HTTP/1.0 404 File Not Found\r\n");
 	send(client, buf, sz, 0);
 	
-	sz = sprintf(buf, "Content-Type: text/html\r\n");
+	sz = snprintf(buf, MAX_BUFFER_ERROR, "Content-Type: text/html\r\n");
 	send(client, buf, sz, 0);
 	
-	sz = sprintf(buf, "\r\n");
+	sz = snprintf(buf, MAX_BUFFER_ERROR, "\r\n");
 	send(client, buf, sz, 0);
 	
-	sz = sprintf(buf, "<html><title>Not Found</title><body><p>The server could not find the resourse</p></body></html>");
+	sz = snprintf(buf, MAX_BUFFER_ERROR, "<html><title>Not Found</title><body><p>The server could not find the resourse</p></body></html>");
 	send(client, buf, sz, 0);
 }
 
--- errors.h
+++ errors.h
@@ -2,5 +2,7 @@
 #include <string.h>
 #include <sys/socket.h>
 
+#define MAX_BUFFER_ERROR	1024
+
 void badRequest(int client);
 void notFound(int client);
