c55e6548070ce0126e7204386ce76e49e10b7808
Andrei Jianu <andreisystem@gmail.com>
2012-05-05 22:52:30 +0200

    organizing code

--- server.c
+++ server.c
@@ -1,13 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#include <unistd.h>
-#include <pthread.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-
 #include "headers.h"
 #include "functions.h"
 
