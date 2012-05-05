fc4bf956db1a58fbe666d550f483b9435210b4b7
Andrei Jianu <andreisystem@gmail.com>
2012-05-05 23:07:09 +0200

    organizing code

--- functions.c
+++ functions.c
@@ -45,108 +45,106 @@ FILE *openFile(char *fileName, int sockID)
 
 void acceptPetition(long sockID)
 {
-    struct headers hdr;
-    char buffer[MAX_BUFFER];
-    char ctype[MAX_CTYPE];
-    FILE *pFile = NULL;
+	struct headers hdr;
+	char buffer[MAX_BUFFER];
+	char ctype[MAX_CTYPE];
+	FILE *pFile = NULL;
 
-    memset(ctype, 0, MAX_CTYPE);
-    memset(buffer, 0, MAX_BUFFER);
-    getHeader(&hdr, (int)sockID);
+	memset(ctype, 0, MAX_CTYPE);
+	memset(buffer, 0, MAX_BUFFER);
+	getHeader(&hdr, (int)sockID);
 
-    if(requestMethod(hdr.metode, sockID) == -1)
-    {
-    	pthread_exit(NULL);
-    }
-    
-    if((pFile = openFile(hdr.uri, sockID)) == NULL)
-    {
-    	pthread_exit(NULL);
-    }
+	if(requestMethod(hdr.metode, sockID) == -1)
+	{
+		pthread_exit(NULL);
+	}
 
-    cType(hdr.uri, ctype);
-    fileFound((int) sockID, ctype);
-    int sz = 0;
+	if((pFile = openFile(hdr.uri, sockID)) == NULL)
+	{
+		pthread_exit(NULL);
+	}
 
-    while((sz = fread(buffer, 1, MAX_BUFFER, pFile)) > 0)
-    {
-        send(sockID, buffer, sz, 0);
-    }
+	cType(hdr.uri, ctype);
+	fileFound((int) sockID, ctype);
+	int sz = 0;
 
-    fclose(pFile);
-    close((int) sockID);
-    pthread_exit(NULL);
+	while((sz = fread(buffer, 1, MAX_BUFFER, pFile)) > 0)
+	{
+		send(sockID, buffer, sz, 0);
+	}
+
+	fclose(pFile);
+	close((int) sockID);
+	pthread_exit(NULL);
 }
 
 void getHeader(struct headers *hdr, int sockID)
 {
-    int i = 0;
-    int c = '\n';
-    memset(hdr, 0, sizeof(struct headers));
+	int i = 0;
+	int c = '\n';
+	memset(hdr, 0, sizeof(struct headers));
 
-    for(i = 0; i < HDR_METODE_SZ && (recv(sockID, &c, 1, 0) != -1) && (c != ' ' && c != '\n'); i++)
-    {
-        if(c != '\r')
-        {
-            hdr->metode[i] = c;
-        }
-    }
+	for(i = 0; i < HDR_METODE_SZ && (recv(sockID, &c, 1, 0) != -1) && (c != ' ' && c != '\n'); i++)
+	{
+		if(c != '\r')
+		{
+			hdr->metode[i] = c;
+		}
+	}
 
-    for(i = 0; i < HDR_URI_SZ && (recv(sockID, &c, 1, 0) != -1) && (c != ' ' && c != '\n'); i++)
-    {
-        if(c != '\r')
-        {
-            hdr->uri[i] = c;
-        }
-    }
+	for(i = 0; i < HDR_URI_SZ && (recv(sockID, &c, 1, 0) != -1) && (c != ' ' && c != '\n'); i++)
+	{
+		if(c != '\r')
+		{
+			hdr->uri[i] = c;
+		}
+	}
 
-    
-    if(strlen(hdr->uri) > 1)
-    {
-        for(i = 0; i < strlen(hdr->uri); i++)
-        {
-            hdr->uri[i] = hdr->uri[i + 1];
-        }
-    }
-    else
-    {
-        strncpy(hdr->uri, HTM_INDEX, HDR_URI_SZ - 1);
-    } 
+	if(strlen(hdr->uri) > 1)
+	{
+		for(i = 0; i < strlen(hdr->uri); i++)
+		{
+			hdr->uri[i] = hdr->uri[i + 1];
+		}
+	}
+	else
+	{
+		strncpy(hdr->uri, HTM_INDEX, HDR_URI_SZ - 1);
+	}
 
-    for(i = 0; i < HDR_VERSION_SZ && (recv(sockID, &c, 1, 0) != -1) && (c != ' ' && c != '\n'); i++)
-    {
-
-        if(c != '\r')
-        {
-            hdr->version[i] = c;
-        }
-    }
+	for(i = 0; i < HDR_VERSION_SZ && (recv(sockID, &c, 1, 0) != -1) && (c != ' ' && c != '\n'); i++)
+	{
+		if(c != '\r')
+		{
+			hdr->version[i] = c;
+		}
+	}
 }
 
 void cType(char *fileName, char *cType)
 {
-    char *c = strrchr(fileName, '.');
-    char ext[5]; memset(ext, 0, 5);
-    strncpy(ext, c + 1, 4);
+	char *c = strrchr(fileName, '.');
+	char ext[5]; memset(ext, 0, 5);
+	strncpy(ext, c + 1, 4);
 
-    if(!strcmp(ext, "htm") || !strcmp(ext, "html"))
-    {
-        strcat(cType, "text/html");
-    }
-    else if(!strcmp(ext,"gif"))
-    {
-        strcat(cType, "image/gif");
-    }
-    else if(!strcmp(ext,"jpg") || !strcmp(ext, "jpeg"))
-    {
-        strcat(cType, "image/jpeg");
-    }
-    else if(!strcmp(ext,"png"))
-    {
-        strcat(cType, "image/png");
-    }
-    else if(!strcmp(ext,"txt"))
-    {
-        strcat(cType, "text/plain");
-    }
+	if(!strcmp(ext, "htm") || !strcmp(ext, "html"))
+	{
+		strcat(cType, "text/html");
+	}
+	else if(!strcmp(ext,"gif"))
+	{
+		strcat(cType, "image/gif");
+	}
+	else if(!strcmp(ext,"jpg") || !strcmp(ext, "jpeg"))
+	{
+		strcat(cType, "image/jpeg");
+	}
+	else if(!strcmp(ext,"png"))
+	{
+		strcat(cType, "image/png");
+	}
+	else if(!strcmp(ext,"txt"))
+	{
+		strcat(cType, "text/plain");
+	}
 }
